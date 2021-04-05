c112c9f5bc4b82b53b42657bdd23c5bb107131b5
DabirRahmani <trtpta@gmail.com>
2021-04-05 20:23:03 +0430

    login finished

--- charity_front/src/App.js
+++ charity_front/src/App.js
@@ -3,43 +3,21 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route, useHistory} from 'react-router-dom';
 import Login from './view/scripts/pages/login/login.js';
-
-
-
+import SignUp from './view/scripts/pages/signup/signup';
 
 const App =() =>{
 
     return (<Router>
-      <div className="App" style={{overflowY:"hidden", height:"100vh"}}>
+      <div className="App" style={{overflowY:"hidden"}}>
           <Switch>
               <Route path="/login" component={Login}/>
+              <Route path="/signup" component={SignUp}/>
+
           </Switch>
       </div>
     </Router>);
 
 }
 
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} 
-*/
 
 export default App;
