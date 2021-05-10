d3cfb6231d0be6dd3f40f56811607821d1041ccd
DabirRahmani <trtpta@gmail.com>
2021-05-10 18:08:52 +0430

    conflict03

--- src/core/adminPanel/eventManagerRequest.js
+++ src/core/adminPanel/eventManagerRequest.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import BackendUrl from '../backendUrl'
 
 const GetEventsRequests =({admintoken})=> 
 {
@@ -15,11 +16,15 @@ const ConfrimRequestedEvent =({admintoken, eventid, feedback})=>
     return axios.create({baseURL: BackendUrl()}).post( '/LeaveFeedback', {TOKEN_ID:admintoken,event_id:eventid,feedback:feedback,accept:"1"})
 }
 
-const ModifyRequestedEvent =({admintoken,eventid, title, description,listofneeds,imageurl,feedback })=> 
+const EditRequestForRequestedEvent =({admintoken, eventid, feedback})=> 
 {
-    return axios.create({baseURL: BackendUrl()}).post( '/EditEventByAdmin', {list_of_needs:listofneeds,TOKEN_ID:admintoken,event_id:eventid,feedback:feedback,title:title,description:description,image_url:imageurl})
+    return axios.create({baseURL: BackendUrl()}).post( '/LeaveFeedback', {TOKEN_ID:admintoken,event_id:eventid,feedback:feedback})
+}
 
+const ModifyRequestedEvent =({admintoken,eventid, title, description,listofneeds,imageurl,feedback,moneytarget })=> 
+{
+    return axios.create({baseURL: BackendUrl()}).post( '/EditEventByAdmin', {list_of_needs:listofneeds,TOKEN_ID:admintoken,event_id:eventid,feedback:feedback,title:title,description:description,image_url:imageurl,money_target:moneytarget})
 }
 
 
-export {ConfrimRequestedEvent,DeleteRequestedEvent,ModifyRequestedEvent,GetEventsRequests}
+export {ConfrimRequestedEvent,DeleteRequestedEvent,ModifyRequestedEvent,GetEventsRequests,EditRequestForRequestedEvent}
