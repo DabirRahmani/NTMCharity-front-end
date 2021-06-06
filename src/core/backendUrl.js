6320c8e7b6529a855f54e6b3a801e6690d4ed3b0
DabirRahmani <trtpta@gmail.com>
2021-06-07 01:30:04 +0430

    changebackurl

--- src/core/backendUrl.js
+++ src/core/backendUrl.js
@@ -1,6 +1,7 @@
 const BackendUrl =()=>
 {
-    return "http://185.231.115.150:8000/App1"
+    return "https://cors-anywhere.herokuapp.com/http://185.231.115.150:8000/App1"
+    //return "http://185.231.115.150:8000/App1"
     //return "http://127.0.0.1:8000/App1"
     //return "http://192.168.1.101:8000/App1"
 
