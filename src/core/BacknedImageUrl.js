9019a6ffaed0fd6f3a1e8c47af04b71a1c980fdb
DabirRahmani <trtpta@gmail.com>
2021-07-11 09:53:27 +0430

    chnage back adress

--- src/core/BacknedImageUrl.js
+++ src/core/BacknedImageUrl.js
@@ -1,7 +1,7 @@
 const BackendImageUrl =()=>
 {
-    //return "http://185.231.115.150:8000"
-    return "http://127.0.0.1:8000"
+    return "http://37.152.183.25:8000"
+    //return "http://127.0.0.1:8000"
 
 } 
 export default BackendImageUrl
